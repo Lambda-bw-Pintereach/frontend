b51f199cca27e9ddc1b60e3709489204aeee03a4
Kelly Jerrell <kcjerrell@gmail.com>
2021-06-23 11:24:51 -0700

    preliminary api requests in place

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+
+export function fetchArticles() {
+	return axiosWithAuth().get("/articles");
+}
+
+export function addArticle(article) {
+	return axiosWithAuth().post("/article");
+}
