f1f07e1476377a9f390dc4db9f539ff521313846
Kelly Jerrell <kcjerrell@gmail.com>
2021-06-23 13:10:38 -0700

    meh style

--- src/components/Login.style.js
+++ src/components/Login.style.js
@@ -4,7 +4,7 @@ const LoginContainer = styled.div`
 	label {
 		display: block;
 		position: relative;
-		top: 1.2rem;
+		top: 0.6rem;
 		left: 10px;
 		color: #adbfcf;
 		font-size: 1rem;
@@ -15,39 +15,57 @@ const LoginContainer = styled.div`
 		height: 2.5rem;
 		width: 15rem;
 		font-size: 1.5rem;
-		padding: 1rem .5rem .1rem;
+		padding: .5rem .5rem .1rem;
 		background-color: #444;
 		border: 0;
 		border-radius: 8px 8px 0px 0px;
-		border-bottom: 3px white solid;
+		border-bottom: 3px solid ${p => p.theme.onDark};
 		color: #dddddd;
 	}
 
+	.input-error {
+		border-bottom-color: ${p=>p.theme.redColor} !important;
+	}
+
 	input:focus, input:focus-visible {
-		border-bottom: 3px solid #bf8cf9;
+		border-bottom: 3px solid ${p=>p.theme.blueHigh};
 		outline: 0;
 	}
 
 	.login-error {
-		max-width: 15rem;
-		color: ${p => p.theme.errorOnDark};
-		font-size: .9rem;
+		margin: auto;
+		max-width: 14rem;
+		color: ${p => p.theme.onDark};
+		font-size: 1rem;
+
 	}
 
 	.login-tos {
-		margin: 0em 0em 1em;
-		label, input {
-			display: inline;
+		margin: 1rem auto 0;
+
+		label {
+			position: static;
+			display: flex;
+			flex-direction: row;
+			align-items: center;
 		}
+
+		input[type=checkbox] {
+			height: 1.2rem;
+			width: 1.2rem;
+			margin-right: .5rem;
+		}
+
 	}
 
 	.login-buttons {
+		margin-top: .5rem;
 		display: flex;
 		flex-direction: row-reverse;
 		justify-content: space-evenly;
 
 		* {
-			text-transform: uppercase;
+			text-transform: lowercase;
 		}
 	}
 `;
